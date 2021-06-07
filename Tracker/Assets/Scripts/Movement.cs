8ebc9968d716e0f8161d18374ba8ca0c295f1309
acmkan <34609309+real-acmkan@users.noreply.github.com>
2021-06-07 13:59:43 -0600

    Updated Tracking

--- Tracker/Assets/Scripts/Movement.cs
+++ Tracker/Assets/Scripts/Movement.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Mapbox.Unity.Utilities;
 
 public class Movement : MonoBehaviour
 {
@@ -14,6 +15,8 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
+        // var worldPosition = Conversions.GeoToWorldPosition(51.485852, -113.146361, new Vector2d(10, 10), (float)2.5).ToVector3xz();
         Vec = transform.localPosition;  
         Vec.y += Input.GetAxis("Jump") * Time.deltaTime * 20;  
         if(Input.GetKey(KeyCode.LeftShift)) 
---
+++ Tracker/Assets/Scripts/PathFollower.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathFollower : MonoBehaviour
+{
+    // public Transform[] path;
+    // https://www.youtube.com/watch?v=fvdRKS8x0aM
+}
