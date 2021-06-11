e65710abb7d39691bf892b4041cfa48c0312876f
acmkan <34609309+real-acmkan@users.noreply.github.com>
2021-06-11 13:54:53 -0600

    Updated game

--- Tracker/Assets/Scripts/PathFollower.cs
+++ Tracker/Assets/Scripts/PathFollower.cs
@@ -10,12 +10,19 @@ public class PathFollower : MonoBehaviour
 {
     [SerializeField] public AbstractMap Map;
     [SerializeField] public Vector2d LatLng;
+    [SerializeField] int height;
+    [SerializeField] float step;
 
     private void Update()
     {
         Vector3 position = Conversions.GeoToWorldPosition(LatLng, Map.CenterMercator, Map.WorldRelativeScale).ToVector3xz();
+        Debug.Log(Map.WorldRelativeScale);
 
-        float step = 5 * Time.deltaTime;
-        transform.position = Vector3.MoveTowards(transform.position, position, step);
+        position.z = height;
+        transform.position = Vector3.MoveTowards(transform.position, position, step * Time.deltaTime);
+
+        // if(Vector3.Distance(transform.position, newTarget.position) < 0.1f){
+        //         It is within ~0.1f range, do stuff
+        // }
     }
 }
